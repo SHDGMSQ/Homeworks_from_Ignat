076ee3d9383db47b380c805fed6a972e2b5d7427
SHDGMSQ <sh.dg.msq@mail.ru>
2022-03-05 10:13:58 +0300

    commit after 9 homework

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import HW7 from '../../h7/HW7'
 import HW8 from "../../h8/HW8";
 import HW9 from '../../h9/HW9'
-import HW10 from '../../h10/HW10'
-import HW11 from '../../h11/HW11'
 
 
 function Junior() {
@@ -11,10 +9,10 @@ function Junior() {
         <div>
             <HW7/>
             <HW8/>
-            {/*<HW9/>
-            <HW10/>
-            <HW11/>
-            HW7-HW11*/}
+            <HW9/>
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+            {/*HW7-HW11*/}
 
         </div>
     )
